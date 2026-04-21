e97d2d96df2466bf88ebf4a2b00123a273745783
Lily Hamilton <lily.hamilton43@gmail.com>
2026-04-21 17:53:01 -0400

    Added a distance invalid checker

--- Robot/src/Ultrasonic.cpp
+++ Robot/src/Ultrasonic.cpp
@@ -29,6 +29,21 @@ int getDistance() {
   return validReading;
 }
 
+void invalidCheck(){
+  int numval = 0;
+  for (int i = 0 ; i < 6 ; i++){
+    if (numval == 5){
+      state = 0;
+    }
+    else if (getDistance() == 0){
+      numval++;
+    }
+    else {
+      numval = 0;
+    }
+  }
+}
+
 bool distanceCheck(){
   distance = getDistance();
   if (0 < distance && distance <= MIN_DISTANCE){
